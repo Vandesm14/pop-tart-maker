c1b6dbaab532e4deadfc513acd30037b02325bc8
vandesm14 <vandesm14@gmail.com>
2020-12-31 18:42:00 -0500

    tons of stuff

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+jscolor.presets.default = {
+	palette: '#FFDCA3 #FFFFFF #845100 #523912',
+}
+
 $(document).ready(function () {
 	$('#input-crust').on('change', (e) => $('#crust, #crust_2').css('color', $(e.target).val()));
 	$('#input-icing').on('change', (e) => $('#icing_2').css('color', $(e.target).val()));
@@ -5,4 +9,10 @@ $(document).ready(function () {
 	$('#input-sprinkles-color').on('change', (e) => $('#sprinkles > path').css('color', $(e.target).val()));
 	$('#input-filling').on('change', (e) => $('#main').css('color', $(e.target).val()));
 	$('#input-extra-color').on('change', (e) => $('#secondary').css('color', $(e.target).val()));
+	
+	$('#input-extra').on('change', (e) => $(e.target).prop('checked') ? $('#secondary').show() : $('#secondary').hide());
+	$('#input-drizzle').on('change', (e) => $(e.target).prop('checked') ? $('#drizzle').show() : $('#drizzle').hide());
+	$('#input-sprinkles').on('change', (e) => $(e.target).prop('checked') ? $('#sprinkles > path').show() : $('#sprinkles > path').hide());
+
+	$('#input-save').on('click', () => saveAs(new Blob([new XMLSerializer().serializeToString($('#tart')[0])], {type: 'image/svg+xml'}), 'pop-tart.svg'));
 });
